0a448250aafb0744d111e6d1414a539b6e615e18
Pablo Albrecht <pablo.albrecht@gmail.com>
2023-11-24 10:44:34 -0300

    3 commit

--- src/main/java/ClasesObjeto/Tecnico.java
+++ src/main/java/ClasesObjeto/Tecnico.java
@@ -2,6 +2,10 @@
 package ClasesObjeto;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Tecnico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String nombre;
     private String apellido;
